1798a6e2bffc8281f64d5aa32ffaf18d99db9963
HemaSudha <hemasudha.a@way2news.com>
2023-11-06 17:58:07 +0530

    firat commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React, { useState } from 'react';
 function App() {
+  const [inputvalue, setInputValue] = useState('');
+  const [count, setCount] = useState(0)
+
+  const handleinputchange = (e) => {
+    const text = e.target.value;
+    setInputValue(text)
+
+    // words count
+    const words = text.trim();
+    setCount(words.length)
+  }
+  const handleCount = () => {
+    setCount('');
+    setInputValue('')
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="letterContainer">
+      <div className="letterparent">
+        <div className="letterDetails">
+          <h1 className='heading'>Calculate the Letters You enter</h1>
+          <p>Enter the Phrase</p>
+          <input type="text" placholder="enter something......." value={inputvalue} onChange={handleinputchange} className='letters-input ' />
+          <button className='letters-count' onClick={handleCount}>No.of.letters:{count}</button>
+        </div>
+        <div className='image-section'>
+          <img src="https://assets.ccbp.in/frontend/react-js/stop-watch-with-calculator-img.png" alt="letter-logo" className="letter-logo" />
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
